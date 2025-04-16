acdc80d90c14035e3dd4478c18e701dfdf392c3e
rohanchauhan02 <rohan021195@gmail.com>
2025-04-16 23:25:07 +0530

    adde support of slice

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/ocuris/container/heap"
 )
 
 func main() {
 
-	pq := heap.NewMax[int](3)
+	pq := heap.NewOrdered[int]()
 	pq.Push(5)
 	pq.Push(3)
 	pq.Push(8)
@@ -24,4 +25,39 @@ func main() {
 	for pq.Len() > 0 {
 		fmt.Println(pq.Pop())
 	}
+
+	lengthComparator := func(a, b string) int {
+		return len(a) - len(b) // Shorter strings have higher priority
+	}
+
+	pq1 := heap.New(lengthComparator)
+	pq1.Push("go")
+	pq1.Push("rust")
+	pq1.Push("python")
+
+	for pq1.Len() > 0 {
+		val, _ := pq1.Pop()
+		fmt.Println(val) // Output: go, rust, python
+	}
+
+	type Task struct {
+		Priority int
+		Name     string
+	}
+
+	pq2 := heap.New(func(a, b Task) int {
+		if a.Priority != b.Priority {
+			return a.Priority - b.Priority // Higher priority first
+		}
+		return strings.Compare(a.Name, b.Name)
+	})
+	pq2.Push(Task{2, "Write docs"})
+	pq2.Push(Task{1, "Fix bug"})
+	pq2.Push(Task{2, "Review code"})
+
+	for pq2.Len() > 0 {
+		task, _ := pq2.Pop()
+		fmt.Printf("%d: %s\n", task.Priority, task.Name)
+	}
+
 }
